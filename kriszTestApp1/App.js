b13043d804d8c55a50d225f104c70a6423f20d83
Krisztián Kecskés <kecskesk@sonrisa.hu>
2017-11-04 14:11:29 +0100

    older version made on dell

--- kriszTestApp1/App.js
+++ kriszTestApp1/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, FlatList, Button, Alert } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Button, Alert, TextInput, KeyboardAvoidingView  } from 'react-native';
 import * as firebase from 'firebase';
 
 export default class App extends React.Component {
@@ -23,7 +23,33 @@ export default class App extends React.Component {
     };
 
     addItem = () => {
-        this.itemsRef.push({ color: this.getRandomColor(), id: this.state.dataSource.length + 1, name: 'newName in ' + this.getRandomColor() });
+        this.itemsRef.push({ color: this.getRandomColor(),
+            id: this.state.dataSource.length + 1,
+            name: this.state.itemName ?
+              this.state.itemName : 'newName in ' + this.getRandomColor() }).then(() => {
+                if(this.myFlatList) {
+                    this.myFlatList.scrollToEnd();
+                    this.setState({itemName: ''});
+                }
+              });
+    };
+
+    removeAllItems = () => {
+        Alert.alert(
+            'Remove all items',
+            'Are you sure, you want to delete all items?',
+            [
+              {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+              {text: 'OK', onPress: () => this.itemsRef.remove()},
+            ],
+            { cancelable: false }
+          );
+    };
+
+    removeItem = (key) => {
+        if (key && this.itemsRef.child(key)) {
+            this.itemsRef.child(key).remove();
+        }
     };
 
     componentDidMount() {
@@ -54,34 +80,91 @@ export default class App extends React.Component {
         }
     }
 
+    renderListItem = (item) => {
+        return (
+            <View style={styles.listRow}>
+                <Button style={styles.listCell} onPress={() => this.removeItem(item._key)} title="DEL" />
+                <Text style={styles.listCell}>
+                    {item.id}
+                </Text>
+                <Text style={[styles.listCell, {color: item.color}]}>
+                    {item.name}
+                </Text>
+                <Text style={[styles.listCell, {color: item.color}]}>
+                    (pszt: {item._key})
+                </Text>
+            </View>
+        );
+    };
+
     render() {
     return (
-        <View style={styles.container}>
+        <KeyboardAvoidingView behavior="padding" style={styles.container}>
 
             <Text style={styles.title}>Grocery List</Text>
 
             <FlatList
+                ref={(list) => this.myFlatList = list}
                 data={this.state.dataSource}
-                renderItem={({item}) => <Text style={{color: item.color}}>* {item.id} * {item.name} (pszt: {item._key})</Text>}
+                renderItem={({item}) => this.renderListItem(item)}
                 keyExtractor={(item, index) => index}/>
 
-            <Button onPress={this.addItem} title="Add" />
+            <View style={styles.addRow}>
+                <TextInput
+                  value={this.state.itemName}
+                  style={[styles.listCell, styles.itemNameInput]}
+                  placeholder="name for new item"
+                  onChangeText={(text) => this.setState({itemName: text})}/>
 
-        </View>
+                  <View style={[styles.listCell, styles.actionButton]} >
+                    <Button onPress={this.addItem} title="add" />
+                  </View>
+                  <View style={[styles.listCell, styles.actionButton]} >
+                    <Button onPress={this.removeAllItems} title="empty" />
+                  </View>
+            </View>
+        </KeyboardAvoidingView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-      marginBottom:30
-  },
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 10
+    },
     title: {
-      marginTop:30
+      fontStyle: 'italic',
+      marginTop: 40,
+      marginBottom: 10
+    },
+    itemNameInput: {
+      flex: 5,
+      height: 40,
+      padding: 5
+    },
+    listRow: {
+        flex: 1,
+        flexDirection: 'row',
+        margin: 1,
+        padding: 4,
+        borderWidth: 1,
+        borderColor: 'black'
+    },
+    actionButton: {
+      margin: 1,
+
+    },
+    listCell: {
+        margin: 10
+    },
+    addRow: {
+      flexDirection: 'row',
+      height: 50,
+      alignItems: 'center',
+      justifyContent: 'center'
     }
 });
 
